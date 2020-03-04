fcfdca69aadb4f637edb04bf3340fdead250343a
hirawaseem <amjadalifarooq7@gamil.com>
2020-03-04 15:57:36 +0500

    dawat

---
+++ script.js
@@ -0,0 +1,24 @@
+// var number = 1;
+// var addition = --number;
+// number = (8 / 4) + 2 * 3 -1;
+// var firstName = prompt("First Name:" , "");
+// var lastName = prompt("Last Name" , "");
+
+// alert (firstName + " " + lastName + 123);
+// alert (number);
+
+// var tatalAttendance = 
+// prompt("Days" , "");
+// prompt("Your Full Name:" , "");
+// var yourFullName = firstName + " " + lastName;
+// alert ("Your full name is" + " " + yourFullName)
+
+var kahanSeho = prompt("Kahan se ho ?" , "")    
+
+if(kahanSeho === "pakistan se"){
+    alert("psl to dekhty hogy?")
+
+}
+else{
+    alert("psl se toh jalti hogi?")
+}
